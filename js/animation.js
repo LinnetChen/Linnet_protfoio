3bf36b8669182732ad148bab8a12119fede267f9
Linnet <linnet_chen@digeam.com>
2025-05-11 21:37:38 +0800

    20250511-2137

--- js/animation.js
+++ js/animation.js
@@ -5,24 +5,15 @@ gsap.registerPlugin(ScrollTrigger);
   { target: ".secTitle", props: { scale: 0.5 } },
   { target: ".section_1_box", props: { yPercent: 0 } },
   { target: ".banner", props: { yPercent: 10 } },
-  { target: ".secTextUiux", props: { yPercent: -5 } },
 ].forEach(({ target, props }) => gsap.set(target, props));
 
-// 首頁標題動畫（進場就觸發）
-gsap.to(".mainTitle1", {
-  xPercent: 200,
-  scale: 5,
-  opacity: 0,
-  ease: "none",
-  scrollTrigger: { scrub: 1 },
-});
+
 
 // 滾動動畫統一配置
 const scrollAnimations = [
   { selector: ".secTitle",    animation: { yPercent: 5, scale: 1.2 } },
   { selector: ".section_1_box",  animation: { yPercent: 10, scale: 1 } },
   { selector: ".banner",      animation: { yPercent: -10, scale: 1 } },
-  { selector: ".secTextUiux", animation: { yPercent: 25, scale: 1 } },
 ];
 
 // 套用每組滾動動畫
